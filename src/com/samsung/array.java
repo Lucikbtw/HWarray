f4b57d9ef038327615c7bca11490efd644b15874
Lucikbtw <fouraj.4aj@gmail.com>
2024-10-09 18:55:01 +0300

    task7

--- src/com/samsung/array.java
+++ src/com/samsung/array.java
@@ -6,5 +6,33 @@ public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int N = scanner.nextInt();
+        int[] arr = new int[N];
+
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+
+        List<Integer> negativeList = new ArrayList<>();
+        List<Integer> nonNegativeList = new ArrayList<>();
+
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] < 0) {
+                negativeList.add(arr[i]);
+            } else {
+                nonNegativeList.add(arr[i]);
+            }
+        }
+
+
+        for (int num : negativeList) {
+            System.out.print(num + " ");
+        }
+        for (int num : nonNegativeList) {
+            System.out.print(num + " ");
+        }
     }
 }
