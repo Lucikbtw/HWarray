05b1063ddeb45f5ee653d2903396637bc11b2f82
Lucikbtw <fouraj.4aj@gmail.com>
2024-10-09 18:35:54 +0300

    task4

--- src/com/samsung/array.java
+++ src/com/samsung/array.java
@@ -6,5 +6,16 @@ public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+        int num = 4;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = num;
+            num = num + 3;
+        }
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
     }
 }
