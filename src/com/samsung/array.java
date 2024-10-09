32b44232ac3eb27eb1a17d35b2cdf48b601fffac
Lucikbtw <fouraj.4aj@gmail.com>
2024-10-09 18:19:48 +0300

    task2

--- src/com/samsung/array.java
+++ src/com/samsung/array.java
@@ -6,5 +6,27 @@ public class array {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        int sum = 0;
+        boolean flag = false;
+        for (int j : array) {
+            if (j % 2 == 0) {
+                flag = true;
+                sum = sum + j;
+            }
+        }
+
+        if (flag) {
+            System.out.println(sum);
+        }
+        else {
+            System.out.println("NO");
+        }
     }
 }
